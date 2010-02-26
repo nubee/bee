d6361aa06a24d413c83b97122dae43884bb95dae
U-MONDIDINAMICI\patrizia.ligas <patrizia.ligas@CADEV306.(none)>
2010-02-26 15:10:59 +0100

    Modified nbOption and relative tests.

--- lib/core/option/nbOption.php
+++ lib/core/option/nbOption.php
@@ -5,17 +5,18 @@ class nbOption
   const PARAMETER_NONE     = 1;
   const PARAMETER_REQUIRED = 2;
   const PARAMETER_OPTIONAL = 4;
+  const IS_ARRAY = 8;
 
-  private $name, $shortcut, $mode;
+  private $name, $shortcut, $mode, $value;
 
-  public function  __construct($name, $shortcut = null, $mode = nbOption::PARAMETER_NONE )
+  public function  __construct($name, $shortcut = null, $mode = self::PARAMETER_NONE, $default = null )
   {
     if(null === $name || "" === $name)
     {
       throw new InvalidArgumentException("Option name is null or empty");
     }
 
-    if(is_string($mode) || $mode > 7)
+    if(is_string($mode) || $mode > 15)
     {
       throw new InvalidArgumentException("Option accepts only defined modes");
     }
@@ -23,6 +24,7 @@ class nbOption
     $this->name = $name;
     $this->shortcut = $shortcut;
     $this->mode = $mode;
+    $this->setDefault($default);
   }
 
   public function getShortcut()
@@ -31,12 +33,35 @@ class nbOption
   }
   public function hasOptionalParameter()
   {
-    return (nbOption::PARAMETER_OPTIONAL === $this->mode);
+    return self::PARAMETER_OPTIONAL === (self::PARAMETER_OPTIONAL & $this->mode);
   }
 
   public function hasRequiredParameter()
   {
-    return (nbOption::PARAMETER_REQUIRED === $this->mode);
+    return self::PARAMETER_REQUIRED === (self::PARAMETER_REQUIRED & $this->mode);
   }
 
+  public function isArray()
+  {
+    return self::IS_ARRAY === (self::IS_ARRAY & $this->mode);
+  }
+
+  public function setValue($v)
+  {
+    $this->value = $v;
+  }
+
+  public function getValue()
+  {
+    return $this->value;
+  }
+
+  private function setDefault($default) {
+    if(null === $default)
+      return;
+    if($this->hasOptionalParameter())
+      $this->value = $default;
+    else
+      throw new LogicException("Can't pass default value for non optional parameter");
+  }
 }
--- test/unit/option/nbOptionTest.php
+++ test/unit/option/nbOptionTest.php
@@ -2,7 +2,7 @@
 
 require_once dirname(__FILE__) . '/../../bootstrap/unit.php';
 
-$t = new lime_test(10);
+$t = new lime_test(23);
 
 $t->comment("nbOptionTest");
 
@@ -60,4 +60,52 @@ catch(Exception $e)
   $t->pass("__constructor() accept only defined modes");
 }
 
+$t->comment("->isArray()");
 
+$option = new nbOption('foo','f', nbOption::PARAMETER_OPTIONAL);
+$t->is($option->isArray(), false, '->isArray() returns false if option parameter is not an array');
+
+$option = new nbOption('foo','f', nbOption::PARAMETER_OPTIONAL | nbOption::IS_ARRAY);
+$t->is($option->hasOptionalParameter(), true, '->hasOptionalParameter() returns true if option has optional array parameter');
+$t->is($option->isArray(), true, '->isArray() returns true if option parameter is an array');
+
+$option = new nbOption('foo','f', nbOption::PARAMETER_REQUIRED | nbOption::IS_ARRAY);
+$t->is($option->hasRequiredParameter(), true, '->hasRequiredParameter() returns true if option has required array parameter');
+$t->is($option->isArray(), true, '->isArray() returns true if option parameter is an array');
+
+$option = new nbOption('foo','f', nbOption::PARAMETER_OPTIONAL);
+$option->setValue("bar");
+$t->is($option->getValue(), "bar", '->getValue() returns a value if it is set');
+
+$option = new nbOption('foo','f', nbOption::PARAMETER_OPTIONAL);
+$t->is($option->getValue(), null, '->getValue() returns a value if it is set');
+
+$option = new nbOption('foo','f', nbOption::PARAMETER_OPTIONAL);
+$option->setValue("bar car");
+$t->is($option->getValue(), "bar car", '->getValue() returns a value if it is set');
+
+$option = new nbOption('foo','f', nbOption::PARAMETER_OPTIONAL);
+$t->is($option->getValue(), null, '->getValue() returns null if it is not set');
+
+$option = new nbOption('foo','f', nbOption::PARAMETER_OPTIONAL, 'default');
+$t->is($option->getValue(), 'default', '->getValue() returns a value if it is set');
+
+try
+{
+  $option = new nbOption('foo','f', nbOption::PARAMETER_NONE, 'default');
+  $t->fail("Can't pass default value for non optional parameter");
+}
+catch(Exception $e)
+{
+  $t->pass("Can't pass default value for non optional parameter");
+}
+
+try
+{
+  $option = new nbOption('foo','f', nbOption::PARAMETER_REQUIRED, 'default');
+  $t->fail("Can't pass default value for non optional parameter");
+}
+catch(Exception $e)
+{
+  $t->pass("Can't pass default value for non optional parameter");
+}
