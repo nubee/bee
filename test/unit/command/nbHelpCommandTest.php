769e552ae792e5c3ea5b33474afdd5e226fb05a9
mambu <marco.ambu@CADEV102.(none)>
2010-03-02 11:49:44 +0100

    Added nbHelpCommandTest

--- test/unit/command/nbHelpCommandTest.php
+++ test/unit/command/nbHelpCommandTest.php
@@ -1,1 +1,17 @@
 <?php
+
+require_once dirname(__FILE__) . '/../../bootstrap/unit.php';
+
+$t = new lime_test(2);
+
+$output = new nbStreamOutput();
+
+$cmd = new nbHelpCommand();
+$cmd->setOutput($output);
+
+$t->is($cmd->getName(), 'help', '->getName() is "help"');
+
+//print_r($argv);
+
+$cmd->run(new nbCommandLineParser(), '');
+$t->is($output->getStream(), 'help');
