5d64b6f0a08e0cc049347cad806c36a72662089f
patrizia.ligas <patrizia.ligas@mondidinamici.com>
2010-03-02 12:37:08 +0100

    nbYamlConfigurationParser now is stateless and add automagically to nbConfiguration all parsed strings and files

--- lib/core/configuration/nbYamlConfigurationParser.php
+++ lib/core/configuration/nbYamlConfigurationParser.php
@@ -1,30 +1,20 @@
 <?php
 class nbYamlConfigurationParser
 {
-  private $yamlParser = null, $result = array();
+  private $yamlParser = null;
+
   public function __construct()
   {
     $this->yamlParser = new sfYamlParser();
   }
 
-  public function get()
-  {
-    return $this->result;
-  }
-
   public function parse($yamlString)
   {
-    $this->result = array_merge($this->result, $this->yamlParser->parse($yamlString));
+    nbConfiguration::add($this->yamlParser->parse($yamlString));
   }
 
   public function parseFile($filePath)
   {
     $this->parse(file_get_contents($filePath));
   }
-
-  public function clear()
-  {
-    $this->result = array();
-  }
-
 }
