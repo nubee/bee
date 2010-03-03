39644061676631411b4e3c3265fe4d95f16d0d82
pietrino.atzeni <pietrino.atzeni@mondidinamici.com>
2010-03-03 14:39:07 +0100

    fixed logger tests

--- test/unit/core/logger/nbLoggerTest.php
+++ test/unit/core/logger/nbLoggerTest.php
@@ -19,4 +19,5 @@ $t->is(nbLogger::formatLevel(nbLogger::INFO), 'INFO', '->formatLevel() has INFO 
 $t->is(nbLogger::formatLevel(nbLogger::COMMENT), 'COMMENT', '->formatLevel() has COMMENT level');
 $t->is(nbLogger::formatLevel(nbLogger::QUESTION), 'QUESTION', '->formatLevel() has QUESTION level');
 
-$t->is($logger->format('text', nbLogger::INFO), '<info>text</info>', '->format() formats "text" as "<info>text</info"');
+//$t->is($logger->format('text', nbLogger::INFO), '<info>text</info>', '->format() formats "text" as "<info>text</info"');
+$t->is($logger->format('text', 'info'), '<info>text</info>', '->format() formats "text" as "<info>text</info"');
