8b476129a54d7c24355af75215f5dbcfae3dc3a3
unknown <nicola.piras@.(none)>
2010-03-05 11:07:35 +0100

    Modified nbShell

--- lib/core/shell/nbShell.php
+++ lib/core/shell/nbShell.php
@@ -9,6 +9,7 @@ class nbShell
 
   public function __construct($redirectOutput = false)
   {
+    echo sprintf("output redirected: %s\n", ($redirectOutput ? 'true' : 'false'));
     $this->redirectOutput = $redirectOutput;
   }
 
@@ -21,22 +22,42 @@ class nbShell
 
 //    echo "test";
     $descriptors = array();
-    if($this->redirectOutput) {
+//    if($this->redirectOutput) {
       $descriptors[] = array("pipe", "r");  // stdin is a pipe that the child will read from
       $descriptors[] = array("pipe", "w");  // stdout is a pipe that the child will write to
       $descriptors[] = array("pipe", "w");   // stderr is a pipe that the child will write to
-    }
+/*    }
     else {
       $descriptors[] = STDIN;
       $descriptors[] = STDOUT;
       $descriptors[] = STDERR;
     }
-
+*/
     $process = proc_open($command, $descriptors, $pipes);
     if(!is_resource($process))
       throw new LogicException('Process cannot be spawned');
 
-    if($this->redirectOutput) {
+    while(($stdout = !feof($pipes[1])) || ($stderr = !feof($pipes[2]))) {
+      if(true === $this->redirectOutput) {
+        if($stdout)
+          $this->output .= fgets($pipes[1]);
+        if($stderr)
+          $this->error .= fgets($pipes[2]);
+      } else {
+        if($stdout) {
+          $outputLine = fgets($pipes[1]);
+          $this->output .= $outputLine;
+          echo $outputLine;
+        }
+        if($stderr) {
+          $errorLine = fgets($pipes[2]);
+          $this->error .= $errorLine;
+          echo $errorLine;
+        }
+      }
+    }
+
+/*    if($this->redirectOutput) {
       fclose($pipes[0]);
       
       $this->output = stream_get_contents($pipes[1]);
@@ -45,7 +66,7 @@ class nbShell
       $this->error = stream_get_contents($pipes[2]);
       fclose($pipes[2]);
     }
-
+*/
     $this->returnCode = proc_close($process);
     if(0 !== $this->returnCode) {
       throw new LogicException(sprintf(
