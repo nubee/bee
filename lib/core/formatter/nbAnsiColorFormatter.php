37a86cf2793be06a39fa27125a71b93cfbdf77cd
Andrea Puggioni <andrea.puggioni@mondidinamici.com>
2010-04-23 18:33:01 +0200

    removed static from nbAnsiColorFormatter::style

--- lib/core/formatter/nbAnsiColorFormatter.php
+++ lib/core/formatter/nbAnsiColorFormatter.php
@@ -3,22 +3,23 @@
 class nbAnsiColorFormatter extends nbFormatter
 {
   protected
-    static $styles = array(
+    $styles = array(
       'error'    => array('bg' => 'red', 'fg' => 'white', 'bold' => true),
       'info'     => array('fg' => 'green', 'bold' => true),
       'comment'  => array('fg' => 'yellow'),
       'question' => array('bg' => 'cyan', 'fg' => 'black', 'bold' => false),
-    ),
+    );
+  protected static
     $options    = array('bold' => 1, 'underscore' => 4, 'blink' => 5, 'reverse' => 7, 'conceal' => 8),
     $foreground = array('black' => 30, 'red' => 31, 'green' => 32, 'yellow' => 33, 'blue' => 34, 'magenta' => 35, 'cyan' => 36, 'white' => 37),
     $background = array('black' => 40, 'red' => 41, 'green' => 42, 'yellow' => 43, 'blue' => 44, 'magenta' => 45, 'cyan' => 46, 'white' => 47);
 
   protected function replaceStartStyle($match)
   {
-    if (!isset(self::$styles[strtolower($match[1])]))
+    if (!isset($this->styles[strtolower($match[1])]))
       throw new InvalidArgumentException(sprintf('[nbAnsiColorFormatter::replaceStartStyle] Unknown style "%s".', $match[1]));
 
-    $parameters = self::$styles[strtolower($match[1])];
+    $parameters = $this->styles[strtolower($match[1])];
     $codes = array();
 
     if (isset($parameters['fg']))
