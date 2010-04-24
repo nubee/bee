1db8f8870045c34a20020b616b4791c599ee121b
Andrea Puggioni <andrea.puggioni@gmail.com>
2010-04-24 09:13:19 +0200

    nbAnsiColorFormatter support for nested tags

--- lib/core/formatter/nbFormatter.php
+++ lib/core/formatter/nbFormatter.php
@@ -4,13 +4,20 @@ class nbFormatter
 {
   public function format($message)
   {
-    $message = preg_replace_callback('#<([a-z][a-z0-9\-_]+)>#i', array($this, 'replaceStartStyle'), $message);
-
-    return preg_replace_callback('#</([a-z][a-z0-9\-_]+)>#i', array($this, 'replaceEndStyle'), $message);
+    return preg_replace_callback('#<(/?)([a-z][a-z0-9\-_]+)>#i', array($this, 'replaceStyle'), $message);
+//    $message = preg_replace_callback('#<([a-z][a-z0-9\-_]+)>#i', array($this, 'replaceStartStyle'), $message);
+//    return preg_replace_callback('#</([a-z][a-z0-9\-_]+)>#i', array($this, 'replaceEndStyle'), $message);
   }
 
   public function formatLine($text) {
     return $this->format($text) + "\n\r";
+  }
+
+  protected function replaceStyle($matches)
+  {
+    if($matches[1] == '/')
+      return $this->replaceEndStyle($matches[1]);
+    return $this->replaceStartStyle($matches[2]);
   }
 
   protected function replaceStartStyle($match)
--- test/unit/core/formatter/nbAnsiColorFormatterTest.php
+++ test/unit/core/formatter/nbAnsiColorFormatterTest.php
@@ -2,9 +2,11 @@
 
 require_once dirname(__FILE__) . '/../../../bootstrap/unit.php';
 
-$t = new lime_test(2);
+$t = new lime_test(3);
 
 $formatter = new nbFormatter();
 $formatter = new nbAnsiColorFormatter();
 $t->is($formatter->format("before <info>inside</info> after"), "before \033[32;1minside\033[0m after", 'Output is formatted');
 $t->is($formatter->format("<comment>first</comment> <info>second</info>"), "\033[33mfirst\033[0m \033[32;1msecond\033[0m", 'Output is formatted twice');
+
+$t->is($formatter->format("<error><comment>first</comment> <info>second</info></error>"), "\033[37;41;1m\033[33mfirst\033[37;41;1m \033[32;1msecond\033[37;41;1m\033[0m", 'Output with nested tags is correctly formatted ');
