25af098154eba99555908d6b0943815b732f2b83
Stefano Orru <brakko@gmail.com>
2011-06-07 12:29:28 +0200

    bee install command

---
+++ lib/command/bee/nbBeeInstallCommand.php
@@ -0,0 +1,63 @@
+<?php
+
+class nbBeeInstallCommand extends nbCommand {
+
+  protected function configure() {
+    $this->setName('bee:install')
+            ->setBriefDescription('Install bee')
+            ->setDescription(<<<TXT
+The <info>{$this->getFullName()}</info> command:
+
+  <info>./bee {$this->getFullName()}</info>
+TXT
+    );
+
+    $this->setArguments(new nbArgumentSet(array(
+                new nbArgument('source_dir', nbArgument::OPTIONAL, 'Source directory','.'),
+                new nbArgument('install_dir', nbArgument::OPTIONAL, 'Installation directory',$this->getDefaultInstallDir())
+            )));
+
+    $this->setOptions(new nbOptionSet(array(
+            )));
+  }
+
+  protected function execute(array $arguments = array(), array $options = array()) {
+    $installDir = nbFileSystemUtils::sanitize_dir($arguments['install_dir']);
+    $sourceDir =  nbFileSystemUtils::sanitize_dir($arguments['source_dir']);
+
+    if (!file_exists($installDir))
+      nbFileSystem::mkdir($installDir);
+
+    $this->logLine('Installing bee on folder ' . $installDir, nbLogger::COMMENT);
+    try{
+      $shell = new nbShell();
+      if(PHP_OS == "Linux"){
+        $shell->execute( 'cp -rf '.$sourceDir.'/* '.$installDir.'/');
+        $shell->execute( 'ln -s '.$installDir.'/bee /usr/bin/bee');
+      }
+      else if(PHP_OS == "WINNT"){
+        if ($sourceDir == '.' ) $sourceDir = '*';
+        $shell->execute( 'xcopy '.$sourceDir.' '.$installDir.'\ /E /Y');
+        $this->logLine('Remember to add '.$installDir.' to your Path enviroment variable', nbLogger::COMMENT);
+      }
+      else
+        throw new Exception("Operating System not supported");
+      $this->logLine('Bee successfully installed', nbLogger::COMMENT);
+    }
+    catch(Exception $e){
+      $this->logLine('Error installing bee: '.$e->getMessage(), nbLogger::COMMENT); 
+      throw $e;
+    }
+  }
+
+  protected function getDefaultInstallDir() {
+      if(PHP_OS == "Linux"){
+        return "/var/source/bee";
+      }
+      else if(PHP_OS == "WINNT"){
+        return "%ProgramFiles%\bee";
+      }
+      else
+        throw new Exception("Operating System not supported");
+  }
+}
---
+++ test/unit/command/bee/nbBeeInstallCommandTest.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once dirname(__FILE__) . '/../../../../test/bootstrap/unit.php';
+nbConfig::set('nb_command_dir', nbConfig::get('nb_sandbox_dir'));
+/*
+#nbFileSystem::rmdir(nbConfig::get('nb_test_installation_dir'), true);
+$shell = new nbShell();
+if (PHP_OS == "Linux") {
+  $shell->execute( 'rm -rf '.nbConfig::get('nb_test_installation_dir'));
+}
+else if (PHP_OS == "WINNT") {
+  $shell->execute( 'rd /S /Q '.nbConfig::get('nb_test_installation_dir'));
+}
+
+$t = new lime_test(8);
+$cmd = new nbBeeInstallCommand();
+$cmd->run(new nbCommandLineParser(), './ ' . nbConfig::get('nb_test_installation_dir'));
+$t->ok(file_exists(nbConfig::get('nb_test_installation_dir') . '/config'), 'Command create config directory in installation folder');
+$t->ok(file_exists(nbConfig::get('nb_test_installation_dir') . '/data'), 'Command create data directory in installation folder');
+$t->ok(file_exists(nbConfig::get('nb_test_installation_dir') . '/documentation'), 'Command create documentatio directory in installation folder');
+$t->ok(file_exists(nbConfig::get('nb_test_installation_dir') . '/lib'), 'Command create lib directory in installation folder');
+$t->ok(file_exists(nbConfig::get('nb_test_installation_dir') . '/plugin'), 'Command create plugin directory in installation folder');
+$t->ok(file_exists(nbConfig::get('nb_test_installation_dir') . '/test'), 'Command create test directory in installation folder');
+$t->ok(file_exists(nbConfig::get('nb_test_installation_dir') . '/bee'), 'Command create bee file in installation folder');
+
+if (PHP_OS == "Linux") {
+  $t->ok(file_exists('/usr/bin/bee'), 'Command create symbolic link bee /usr/bin');
+}
+else if (PHP_OS == "WINNT") {
+
+  $t->pass( "TODO: check symbolic link");
+}
+
+*/
