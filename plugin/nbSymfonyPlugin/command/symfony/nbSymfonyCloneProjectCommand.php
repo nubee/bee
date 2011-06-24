2c18f49256c544d9db686d2263c278d43008f320
Pietrino Atzeni <pietrino.atzeni@gmail.com>
2011-06-24 10:28:58 +0200

    Added excludes from project if already exists

--- plugin/nbSymfonyPlugin/command/symfony/nbSymfonyCloneProjectCommand.php
+++ plugin/nbSymfonyPlugin/command/symfony/nbSymfonyCloneProjectCommand.php
@@ -13,75 +13,62 @@ TXT
     );
 
     $this->setArguments(new nbArgumentSet(array(
-        new nbArgument('clone_from', nbArgument::REQUIRED, 'The path of the project to clone.'),
-        new nbArgument('cloned_project_name', nbArgument::REQUIRED, 'The name of the cloned project'),
-      )));
-
-    $this->setOptions(new nbOptionSet(array(
-        new nbOption('clone-to', 'p', nbOption::PARAMETER_REQUIRED, 'The path where clone project'),
-      )));
+      new nbArgument('from', nbArgument::REQUIRED, 'The path of the project to clone.'),
+      new nbArgument('to', nbArgument::REQUIRED, 'The path where to save the clone'),
+      new nbArgument('project-name', nbArgument::REQUIRED, 'The name of the cloned project'),
+    )));
   }
 
   protected function execute(array $arguments = array(), array $options = array()) {
-    $destination = isset($options['clone-to']) ? $options['clone-to'] . '/' : './';
-    $destination = $destination . $arguments['cloned_project_name'] . '/';
+    $destination = $arguments['to'] . '/' . $arguments['project-name'] . '/';
 
-    $filesToCopy = nbFileFinder::create('any')
-      ->discard(array('nbproject'))
-      ->prune(array('nbproject'))
-      ->remove('.')->remove('..')->in($arguments['clone_from']);
+    $exclude = array(
+      'nbproject', '.netbeans', 'cache', 'log'
+    );
+    $files = nbFileFinder::create('any')
+      ->discard($exclude)
+      ->prune($exclude)
+      ->remove('.')
+      ->remove('..')
+      ->in($arguments['from']);
+
+    $cloneExists = is_dir($destination);
+    if($cloneExists) {
+      $finder = nbFileFinder::create('file');
+
+      $files = array_diff($files, $finder->add('*.yml')->in($arguments['from'].'/config'));
+      $files = array_diff($files, $finder->add('*')->in($arguments['from'].'/web/images'));
+      $files = array_diff($files, $finder->add('*.css')->in($arguments['from'].'/web/css'));
+      $files = array_diff($files, $finder->add('*.js')->in($arguments['from'].'/web/js'));
+      $files = array_diff($files, $finder->add('*.yml')->in($arguments['from'].'/apps/frontend/config'));
+      $files = array_diff($files, $finder->add('*.yml')->in($arguments['from'].'/apps/admin/config'));
+    }    
+    $sourceProjectName = basename($arguments['from']);
     
-    preg_match('/\/([^\/]+)$/', $arguments['clone_from'], $result);
-    $projectNameCloneFrom = $result[1];
-    
+    $verbose = isset($options['verbose']) && $options['verbose'];
     $count = 0;
-    foreach ($filesToCopy as $fileToCopy) {
-      $d = preg_replace('/^.+'.$projectNameCloneFrom.'\//', $destination, $fileToCopy);
-      if (is_dir($fileToCopy))
-        nbFileSystem::mkdir($d, true);
-      else
-        nbFileSystem::copy($fileToCopy, $d);
+    foreach ($files as $file) {
+      $dest = preg_replace('/^.+'.$sourceProjectName.'\//', $destination, $file);
+      if (is_dir($file)) {
+        if($verbose) 
+          $this->log('dir+: ' . $dest, sfLogger::INFO);
+        nbFileSystem::mkdir($dest, true);
+      }
+      else {
+        if($verbose) 
+          $this->log('file+: ' . $dest, sfLogger::INFO);
+        nbFileSystem::copy($file, $dest, true);
+      }
       
-      if (($count % 100) == 0)
+      if(!$verbose && ($count++ % 100) == 0)
         $this->log('.');
-
-      $count++;
     }
     
-//    try {
-//      $this->recurse_copy($arguments['clone_from'], $destination);
-//    } catch (Exception $e) {
-//      $this->log($e->getMessage(), nbLogger::ERROR);
-//    }
-    
-//    $finder = nbFileFinder::create('file');
-//
-//    $properties = $finder->add('properties.ini')->remove('.')->remove('..')->in($destination);
-//    $databases = $finder->add('databases.yml')->remove('.')->remove('..')->in($destination);
-//    $files = array_merge($properties, $databases);
-//
-//    nbFileSystem::replaceTokens($projectNameCloneFrom, $arguments['cloned_project_name'], $files);
-    
-    $files = nbFileFinder::create('file')->remove('.')->remove('..')->in($destination);
-    nbFileSystem::replaceTokens($projectNameCloneFrom, $arguments['cloned_project_name'], $files);
+    if(!$cloneExists) {
+      $files = nbFileFinder::create('file')->remove('.')->remove('..')->in($destination);
+      nbFileSystem::replaceTokens($sourceProjectName, $arguments['project-name'], $files);
+    }
     
     return true;
   }
-
-//  protected function recurse_copy($src, $dst) {
-//    $dir = opendir($src);
-//    @mkdir($dst);
-//    while (false !== ( $file = readdir($dir))) {
-//      if (( $file != '.' ) && ( $file != '..' )) {
-//        if (is_dir($src . '/' . $file)) {
-//          $this->recurse_copy($src . '/' . $file, $dst . '/' . $file);
-//        } else {
-//          $this->logLine(sprintf('Coping %s in %s', $src . '/' . $file, $dst), $level);
-//          copy($src . '/' . $file, $dst . '/' . $file);
-//        }
-//      }
-//    }
-//    closedir($dir);
-//  }
-
 }
