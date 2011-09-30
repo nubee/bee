9daeeae972bd9e7e8e718bae03650c21ffefc7aa
nicola.piras <nicola.piras@cancer.di-side>
2011-09-30 10:54:05 +0200

    minor changes

--- plugin/nbSymfony2Plugin/command/symfony2/nbSymfony2DeployStageCommand.php
+++ plugin/nbSymfony2Plugin/command/symfony2/nbSymfony2DeployStageCommand.php
@@ -41,59 +41,57 @@ TXT
       //migrate
       $this->logLine("symfony2:deploy-stage\n\tapply migrations", nbLogger::COMMENT);
       $command = nbConfig::get('symfony2_bin') . ' doctrine:migrations:migrate --no-interaction';
-      if (!$shell->execute($command)) {
-        throw new LogicException(sprintf("
-[nbSymfony2DeployStageCommand::execute] Error executing command:
-  %s
-", $command
-        ));
-      }
+      
+      if (!$shell->execute($command))
+        $this->throwException($command);
     }
 
     if (nbConfig::get('symfony2_exec-cache-clear')) {
       //clear cache
       $this->logLine("symfony2:deploy-stage\n\tclear cache", nbLogger::COMMENT);
       $command = nbConfig::get('symfony2_bin') . ' cache:clear';
-      if (!$shell->execute($command)) {
-        throw new LogicException(sprintf("
-[nbSymfony2DeployStageCommand::execute] Error executing command:
-  %s
-", $command
-        ));
-      }
+      
+      if (!$shell->execute($command))
+        $this->throwException($command);
+    }
+
+    if (nbConfig::get('symfony2_exec-change-owner')) {
+      //change owner
+      $this->logLine("symfony2:deploy-stage\n\tchange owner to 'cache' and 'logs' dirs", nbLogger::COMMENT);
+      $command = 'chown -R www-data:www-data ' . nbConfig::get('symfony2_dir') . '/app/cache ' . nbConfig::get('symfony2_dir') . '/app/logs';
+      
+      if (!$shell->execute($command))
+        $this->throwException($command);
     }
     
-    if (nbConfig::get('symfony2_exec-change-owner-and-mode')) {
-      //clear cache
-      $this->logLine("symfony2:deploy-stage\n\tchange owner and mode to 'cache' and 'logs' dirs", nbLogger::COMMENT);
-      $command = 'chown -R www-data:www-data '.nbConfig::get('symfony2_dir').'/app/cache';
-      $command = 'chown -R www-data:www-data '.nbConfig::get('symfony2_dir').'/app/logs';
-      $command = 'chmod -R 777 '.nbConfig::get('symfony2_dir').'/app/cache';
-      $command = 'chmod -R 777 '.nbConfig::get('symfony2_dir').'/app/logs';
-      if (!$shell->execute($command)) {
-        throw new LogicException(sprintf("
-[nbSymfony2DeployStageCommand::execute] Error executing command:
-  %s
-", $command
-        ));
-      }
+    if (nbConfig::get('symfony2_exec-change-mode')) {
+      //change mode
+      $this->logLine("symfony2:deploy-stage\n\tchange mode to 'cache' and 'logs' dirs", nbLogger::COMMENT);
+      $command = 'chmod -R 777 ' . nbConfig::get('symfony2_dir') . '/app/cache ' . nbConfig::get('symfony2_dir') . '/app/logs';
+      
+      if (!$shell->execute($command))
+        $this->throwException($command);
     }
 
     if (nbConfig::get('symfony2_exec-assets-install')) {
       //install assets
       $this->logLine("symfony2:deploy-stage\n\tinstall assets", nbLogger::COMMENT);
       $command = nbConfig::get('symfony2_bin') . ' assets:install ' . nbConfig::get('symfony2_web-dir');
-      if (!$shell->execute($command)) {
-        throw new LogicException(sprintf("
-[nbSymfony2DeployStageCommand::execute] Error executing command:
-  %s
-", $command
-        ));
-      }
+      
+      if (!$shell->execute($command))
+        $this->throwException($command);
     }
 
     $this->logLine('Done: symfony2:deploy-stage', nbLogger::COMMENT);
     return true;
+  }
+
+  private function throwException($command) {
+    throw new LogicException(sprintf("
+[nbSymfony2DeployStageCommand::execute] Error executing command:
+  %s
+", $command
+    ));
   }
 
 }
