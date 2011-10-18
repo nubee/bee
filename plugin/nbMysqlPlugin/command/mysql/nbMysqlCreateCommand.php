1f4938b17ce587534ac8bc0fe07d6cc44211caf2
Pietrino Atzeni <pietrino.atzeni@gmail.com>
2011-10-18 09:44:29 +0200

    Fixed mysql:create options/arguments

--- plugin/nbMysqlPlugin/command/mysql/nbMysqlCreateCommand.php
+++ plugin/nbMysqlPlugin/command/mysql/nbMysqlCreateCommand.php
@@ -1,7 +1,8 @@
 <?php
 
-class nbMysqlCreateCommand  extends nbCommand
+class nbMysqlCreateCommand extends nbCommand
 {
+
   protected function configure()
   {
     $this->setName('mysql:create')
@@ -11,43 +12,47 @@ The <info>{$this->getFullName()}</info> command:
 
   <info>./bee {$this->getFullName()}</info>
 TXT
-        );
+    );
 
     $this->setArguments(new nbArgumentSet(array(
-      new nbArgument('db-name', nbArgument::REQUIRED, 'Database name'),
-      new nbArgument('mysql-user', nbArgument::REQUIRED, 'Mysql user'),
-      new nbArgument('mysql-user-pwd', nbArgument::OPTIONAL, 'Mysql user password')
-    )));
-    
+        new nbArgument('db-name', nbArgument::REQUIRED, 'New database name'),
+        new nbArgument('mysql-user', nbArgument::REQUIRED, 'Mysql user'),
+        new nbArgument('mysql-password', nbArgument::OPTIONAL, 'Mysql user password')
+      )));
 
     $this->setOptions(new nbOptionSet(array(
-      new nbOption('db-user', '', nbOption::PARAMETER_REQUIRED, 'Database user'),
-      new nbOption('db-user-pwd', '', nbOption::PARAMETER_REQUIRED, 'Database user password')
-    )));
+        new nbOption('user', 'u', nbOption::PARAMETER_REQUIRED, 'New database user'),
+        new nbOption('password', 'p', nbOption::PARAMETER_REQUIRED, 'New database password')
+      )));
   }
 
   protected function execute(array $arguments = array(), array $options = array())
   {
     $shell = new nbShell();
-    $cmd = 'mysqladmin -u '.$arguments['mysql-user']. ' --password='.$arguments['mysql-user-pwd'].' create '. $arguments['db-name'];
+    $mysqlUser = $arguments['mysql-user'];
+    $mysqlPassword = $arguments['mysql-password'];
+    $dbName = $arguments['db-name'];
+
+    $cmd = 'mysqladmin -u ' . $mysqlUser . ' -p ' . $mysqlPassword . ' create ' . $dbName;
     $shell->execute($cmd);
-    $this->logLine('Done - mysql create database');
+    $this->logLine(sprintf('Database %s created', $dbName));
 
-    if(isset($options['db-user'])) {
-      $cmd = 'mysql -u '.$arguments['mysql-user'].' --password='.$arguments['mysql-user-pwd'].
-        ' -e "grant all privileges on '.$arguments['db-name'].'.* to \''.$options['db-user'].'\'@\'localhost\'';
-      
-      if(isset($options['db-user-pwd'])) {
-        $cmd = $cmd.' identified by \''.$options['db-user-pwd'].'\'';   
+    $username = isset($options['username']) ? $options['username'] : null;
+    $password = isset($options['password']) ? $options['password'] : null;
+
+    if($username) {
+      $cmd = 'mysql -u ' . $mysqlUser . ' -p ' . $mysqlPassword .
+        ' -e "grant all privileges on ' . $dbName . '.* to \'' . $username . '\'@\'localhost\'';
+
+      if($password) {
+        $cmd .= ' identified by \'' . $password . '\'';
       }
-      
-      $cmd = $cmd.'"';
-      
-      $shell->execute($cmd);
-      $this->logLine('Done - create database user');
-    }
 
-    return true;
+      $cmd .= '"';
+
+      $shell->execute($cmd);
+      $this->logLine(sprintf('Datebase user %s successfully created', $username));
+    }
   }
 
 }
