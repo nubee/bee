0df77f5a2f25ec25afa295529372e1307f097308
Nicola Piras <nicola.piras.xyz@gmail.com>
2011-10-20 10:50:41 +0200

    fixed nbVersionPlugin

--- plugins/nbVersionPlugin/test/unit/nbUpdateBuildVersionCommandTest.php
+++ plugins/nbVersionPlugin/test/unit/nbUpdateBuildVersionCommandTest.php
@@ -1,17 +1,18 @@
 <?php
 
 require_once dirname(__FILE__) . '/../../../../test/bootstrap/unit.php';
+
 $configParser->parseFile(dirname(__FILE__) . '/../config/config.yml');
 $serviceContainer->pluginLoader->loadPlugins(array('nbVersionPlugin'));
 $versionFile = nbConfig::get('test_version-file');
-$build =  getBuildVersion($versionFile);
+$build = getBuildVersion($versionFile);
 
 $t = new lime_test(2);
 $cmd = new nbUpdateBuildVersionCommand();
 $command_line = $versionFile;
-$t->ok($cmd->run(new nbCommandLineParser(), $command_line), 'Command UpdateBuildVersion called succefully');
+$t->ok($cmd->run(new nbCommandLineParser(), $command_line), 'Command run succefully');
 
-$t->is(getBuildVersion($versionFile),$build + 1,'Build version incremented');
+$t->is(getBuildVersion($versionFile), $build + 1, 'Build version incremented');
 
 function getBuildVersion($versionFile) {
   if (file_exists($versionFile)) {
