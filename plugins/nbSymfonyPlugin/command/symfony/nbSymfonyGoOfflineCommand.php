a95110a07fd26d8fbb4d3db1ab2b1371e67cece0
Nicola Piras <nicola.piras.xyz@gmail.com>
2011-10-20 11:10:39 +0200

    fixed nbSymfonyGoOfflineCommand

--- plugins/nbSymfonyPlugin/command/symfony/nbSymfonyGoOfflineCommand.php
+++ plugins/nbSymfonyPlugin/command/symfony/nbSymfonyGoOfflineCommand.php
@@ -1,9 +1,8 @@
 <?php
 
-class nbSymfonyGoOfflineCommand  extends nbCommand
-{
-  protected function configure()
-  {
+class nbSymfonyGoOfflineCommand extends nbCommand {
+
+  protected function configure() {
     $this->setName('symfony:go-offline')
       ->setBriefDescription('Puts a symfony application offline in a specified enviroment')
       ->setDescription(<<<TXT
@@ -11,27 +10,33 @@ The <info>{$this->getFullName()}</info> command:
 
   <info>./bee {$this->getFullName()}</info>
 TXT
-        );
+    );
 
     $this->setArguments(new nbArgumentSet(array(
-      new nbArgument('symfony_path', nbArgument::REQUIRED, 'Symfony executable path'),
-      new nbArgument('application', nbArgument::REQUIRED, 'Symfony application'),
-      new nbArgument('enviroment', nbArgument::REQUIRED, 'Symfony enviroment')
-    )));
+        new nbArgument('symfony-path', nbArgument::REQUIRED, 'Symfony executable path'),
+        new nbArgument('application', nbArgument::REQUIRED, 'Symfony application'),
+        new nbArgument('enviroment', nbArgument::REQUIRED, 'Symfony enviroment')
+      )));
 
     $this->setOptions(new nbOptionSet(array(
-    )));
+      )));
   }
 
-  protected function execute(array $arguments = array(), array $options = array())
-  {
-      $this->logLine('Site is going offline');
-      $shell = new nbShell();
-      $cmd = 'php '.$arguments['symfony_path'].'/symfony project:disable '.$arguments['application'].' '.$arguments['enviroment'];
-      $this->logLine($cmd);
-      $shell->execute($cmd);
-      $this->logLine('Done - SymfonyGoOfflineCommand');
-      return true;
+  protected function execute(array $arguments = array(), array $options = array()) {
+    $this->logLine('Site is going offline');
+    $shell = new nbShell();
+    $cmd = sprintf('php %s/symfony project:disable %s %s', $arguments['symfony-path'], $arguments['application'], $arguments['enviroment']);
+
+    $this->logLine($cmd);
+    if (!$shell->execute($cmd)) {
+      throw new LogicException(sprintf("
+[nbSymfonyGoOfflineCommand::execute] Error executing command:
+  %s
+", $cmd
+      ));
+    }
+    $this->logLine('Done - SymfonyGoOfflineCommand');
+    return true;
   }
 
 }
