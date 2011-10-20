4a41f878c80c831bd99c6bfd767bae4f9d285ee7
Pietrino Atzeni <pietrino.atzeni@gmail.com>
2011-10-20 08:52:10 +0200

    Fixed symfony:check-dirs

--- plugins/nbSymfonyPlugin/command/symfony/nbSymfonyCheckDirsCommand.php
+++ plugins/nbSymfonyPlugin/command/symfony/nbSymfonyCheckDirsCommand.php
@@ -1,7 +1,8 @@
 <?php
 
-class nbSymfonyCheckDirsCommand  extends nbCommand
+class nbSymfonyCheckDirsCommand extends nbCommand
 {
+
   protected function configure()
   {
     $this->setName('symfony:project-check-dirs')
@@ -11,25 +12,27 @@ The <info>{$this->getFullName()}</info> command:
 
   <info>./bee {$this->getFullName()}</info>
 TXT
-        );
+    );
 
     $this->setArguments(new nbArgumentSet(array(
-      new nbArgument('target_project_path', nbArgument::REQUIRED, 'Symfony target site path'),
-    )));
-
-    $this->setOptions(new nbOptionSet(array(
-    )));
+        new nbArgument('project-dir', nbArgument::REQUIRED, 'Symfony target site dir'),
+      )));
   }
 
   protected function execute(array $arguments = array(), array $options = array())
   {
-      $this->logLine('Checking symfony dirs for '.$arguments['target_project_path']);
-      if(!file_exists($arguments['target_project_path'].'/log'))
-          nbFileSystem::mkdir ($arguments['target_project_path'].'/log');
-      if(!file_exists($arguments['target_project_path'].'/cache'))
-          nbFileSystem::mkdir ($arguments['target_project_path'].'/cache');
-      $this->logLine('Done - Checking symfony dirs');
-      return true;
+    $projectDir = $arguments['project-dir'];
+    $fs = $this->getFileSystem();
+    $this->logLine('Checking symfony dirs for ' . $projectDir);
+
+    if(!file_exists($projectDir . '/log'))
+      $fs->mkdir($projectDir . '/log', true);
+    
+    if(!file_exists($projectDir . '/cache'))
+      $fs->mkdir($projectDir . '/cache', true);
+    
+    $this->logLine('Done - Checking symfony dirs');
+    return true;
   }
 
 }
--- plugins/nbSymfonyPlugin/test/unit/nbSymfonyCheckDirsCommandTest.php
+++ plugins/nbSymfonyPlugin/test/unit/nbSymfonyCheckDirsCommandTest.php
@@ -1,18 +1,19 @@
 <?php
+
 require_once dirname(__FILE__) . '/../../../../test/bootstrap/unit.php';
 $configParser->parseFile(dirname(__FILE__) . '/../config/config.yml');
 $serviceContainer->pluginLoader->loadPlugins(array('nbSymfonyPlugin'));
 
-$logFolder = nbConfig::get('symfony_project-deploy_symfony-exe-path').'/log';
-$cacheFolder = nbConfig::get('symfony_project-deploy_symfony-exe-path').'/cache';
-nbFileSystem::rmdir($logFolder,true);
-nbFileSystem::rmdir($cacheFolder, true);
+$fs = nbFileSystem::getInstance();
+$rootDir = nbConfig::get('symfony_project_deploy_symfony_root_dir');
+$logDir =  $rootDir . '/log';
+$cacheDir = $rootDir . '/cache';
 
 $t = new lime_test(3);
 $cmd = new nbSymfonyCheckDirsCommand();
-$t->ok($cmd->run(new nbCommandLineParser(), nbConfig::get('symfony_project-deploy_symfony-exe-path')),'Command SymfonyCheckDirs called succefully');
-$t->ok(file_exists($logFolder),'Check log dir existence');
-$t->ok(file_exists($cacheFolder),'Check cache dir existence');
+$t->ok($cmd->run(new nbCommandLineParser(), $rootDir), 'Command SymfonyCheckDirs called succefully');
+$t->ok(file_exists($logDir), 'Check log dir existence');
+$t->ok(file_exists($cacheDir), 'Check cache dir existence');
 
-nbFileSystem::rmdir($logFolder,true);
-nbFileSystem::rmdir($cacheFolder, true);
+$fs->rmdir($logDir, true);
+$fs->rmdir($cacheDir, true);
