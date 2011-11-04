12ee6eb1b4586d95bf7c5682246285e01c13801a
Nicola Piras <nicola.piras.xyz@gmail.com>
2011-11-04 12:14:08 +0100

    Added tests to nbMultiChangeModeCommand test

--- plugins/nbFileSystemPlugin/test/bootstrap/unit.php
+++ plugins/nbFileSystemPlugin/test/bootstrap/unit.php
@@ -1,7 +1,12 @@
 <?php
 
 require_once dirname(__FILE__) . '/../../../../test/bootstrap/unit.php';
+
 $dataDir = dirname(__FILE__) . '/../data/config';
+$sandboxDir = dirname(__FILE__) . '/../sandbox';
+
+$fileSystem = nbFileSystem::getInstance();
+
 $configParser->parseFile($dataDir . '/config.yml');
 
 $serviceContainer->pluginLoader->loadPlugins(array('nbFileSystemPlugin'));
--- plugins/nbFileSystemPlugin/test/unit/nbMultiChangeModeCommandTest.php
+++ plugins/nbFileSystemPlugin/test/unit/nbMultiChangeModeCommandTest.php
@@ -5,11 +5,37 @@ require_once dirname(__FILE__) . '/../bootstrap/unit.php';
 $listFile = $dataDir . '/filesystem-multi-change-mode.yml';
 
 if(php_uname('s') == 'Linux') {
-  $t = new lime_test(1);
+  //test setup
+  $testDir1 = $sandboxDir.'/list-item-1';
+  $testDir2 = $sandboxDir.'/list-item-2';
+  $dirname = '/dirname';
+  $filename = '/filename';
+  
+  $fileSystem->mkdir($testDir1.$dirname, true);
+  $fileSystem->touch($testDir1.$filename);
+  $fileSystem->mkdir($testDir2.$dirname, true);
+  $fileSystem->touch($testDir2.$filename);
+  
+  $t = new lime_test(7);
   $cmd = new nbMultiChangeModeCommand();
+  nbConfig::set('test_dir_1', $testDir1);
+  nbConfig::set('test_dir_2', $testDir2);
+  nbConfig::set('dir_mode', '666');
+  nbConfig::set('file_mode', '444');
+  
   $commandLine = sprintf('%s --doit', $listFile);
 
   $t->ok($cmd->run(new nbCommandLineParser(), $commandLine), 'Folder mode changed successfully');
+  $t->is(fileperms($testDir1), '666');
+  $t->is(fileperms($testDir1.$dirname), '666');
+  $t->is(fileperms($testDir1.$filename), '444');
+  $t->is(fileperms($testDir2), '666');
+  $t->is(fileperms($testDir2.$dirname), '666');
+  $t->is(fileperms($testDir2.$filename), '444');
+  
+  //tear down
+  $fileSystem->rmdir($testDir1, true);
+  $fileSystem->rmdir($testDir2, true);
 }
 else {
   $t = new lime_test(1);
