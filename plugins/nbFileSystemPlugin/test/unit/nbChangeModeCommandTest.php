7056b1d3cad4533f07c2001f5c6b233de3589932
Pietrino Atzeni <pietrino.atzeni@di-side.com>
2011-11-04 19:11:12 +0100

    Fixed nbChangeModeCommand tests

--- plugins/nbFileSystemPlugin/test/unit/nbChangeModeCommandTest.php
+++ plugins/nbFileSystemPlugin/test/unit/nbChangeModeCommandTest.php
@@ -4,13 +4,18 @@ require_once dirname(__FILE__) . '/../bootstrap/unit.php';
 
 $mode = '644';
 $folder = nbConfig::get('nb_sandbox_dir') . '/folder';
+$fs = nbFileSystem::getInstance();
 
 if(php_uname('s') == 'Linux') {
+  $fs->mkdir($folder);
+  
   $t = new lime_test(1);
   $cmd = new nbChangeModeCommand();
   $commandLine = sprintf('%s %s', $folder, $mode);
 
   $t->ok($cmd->run(new nbCommandLineParser(), $commandLine), 'Folder mode changed successfully');
+  
+  $fs->rmdir($folder);
 }
 else {
   $t = new lime_test(0);
