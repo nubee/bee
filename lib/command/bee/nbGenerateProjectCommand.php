9b2817446acea8d58000221aa1fb92c2762ca0f2
Stefano Orru <stefano.orru@di-side.com>
2012-01-12 17:08:54 +0100

    add project type label to GenerateProject Command

--- lib/command/bee/nbGenerateProjectCommand.php
+++ lib/command/bee/nbGenerateProjectCommand.php
@@ -1,13 +1,11 @@
 <?php
 
-class nbGenerateProjectCommand extends nbCommand
-{
+class nbGenerateProjectCommand extends nbCommand {
 
-  protected function configure()
-  {
+  protected function configure() {
     $this->setName('bee:generate-project')
-      ->setBriefDescription('Generate a bee project into a folder')
-      ->setDescription(<<<TXT
+            ->setBriefDescription('Generate a bee project into a folder')
+            ->setDescription(<<<TXT
 The <info>{$this->getFullName()}</info> command:
 
   <info>./bee {$this->getFullName()}</info>
@@ -15,28 +13,45 @@ TXT
     );
 
     $this->setArguments(new nbArgumentSet(array(
-        new nbArgument('project-dir', nbArgument::OPTIONAL, 'Project directory', '.'),
-      )));
+                new nbArgument('project-dir', nbArgument::OPTIONAL, 'Project directory', '.'),
+            )));
 
     $this->setOptions(new nbOptionSet(array(
-        new nbOption('force', 'f', nbOption::PARAMETER_NONE, 'Overwrites the existing configuration'),
-      )));
+                new nbOption('force', 'f', nbOption::PARAMETER_NONE, 'Overwrites the existing configuration'),
+                new nbOption('type', 't', nbOption::PARAMETER_REQUIRED, 'define the project type for custom task behavior'),
+            )));
   }
 
-  protected function execute(array $arguments = array(), array $options = array())
-  {
+  protected function execute(array $arguments = array(), array $options = array()) {
+
     $projectDir = $arguments['project-dir'];
     $this->logLine(sprintf('Generating bee project in %s', $projectDir));
-    $fs        = $this->getFileSystem();
+    $fs = $this->getFileSystem();
     $configDir = $fs->sanitizeDir($projectDir) . '/.bee';
-    $force     = isset($options['force']) ? true : false;
+    $force = isset($options['force']) ? true : false;
+    $projectType = isset($options['type']) ? $options['type'] : '';
 
     $fs->mkdir($configDir, true);
     $fs->copy(nbConfig::get('nb_bee_dir') . '/data/config/bee.yml', $configDir . '/bee.yml', $force);
     $fs->copy(nbConfig::get('nb_bee_dir') . '/data/config/config.yml', $configDir . '/config.yml', $force);
 
+    // add project type
+    if ($projectType != '') {
+      $beeConfig = $configDir . '/bee.yml';
+
+      if (!file_exists($beeConfig))
+        throw new Exception($beeConfig . ' not found');
+
+      $configParser = sfYaml::load($beeConfig);
+      $configParser['project']['type'] = $projectType;
+
+      $yml = sfYaml::dump($configParser, 99);
+      
+      file_put_contents($beeConfig, $yml);
+    }
+
     $this->logLine('bee project generated!');
-    
+
     return true;
   }
 
--- test/unit/command/bee/nbGenerateProjectCommandTest.php
+++ test/unit/command/bee/nbGenerateProjectCommandTest.php
@@ -7,8 +7,9 @@ nbConfig::set('nb_plugins_dir', nbConfig::get('nb_test_plugins_dir'));
 $configDir = nbConfig::get('nb_sandbox_dir') . '/.bee';
 $beeYaml = $configDir . '/bee.yml';
 $configYaml = $configDir . '/config.yml';
+$projectType ='foo';
 
-$t = new lime_test(7);
+$t = new lime_test(11);
 
 $cmd = new nbGenerateProjectCommand();
 $t->ok($cmd->run(new nbCommandLineParser(), nbConfig::get('nb_sandbox_dir')), 'Command nbGenerateProjectCommand called successfully');
@@ -28,6 +29,20 @@ $t->ok($cmd->run(new nbCommandLineParser(), '--force ' . nbConfig::get('nb_sandb
 
 $t->ok(file_exists($beeYaml), 'bee.yml added to the destination dir :' . $beeYaml);
 $t->ok(file_exists($configYaml), 'config.yml added to the destination dir :' . $configYaml);
+
+// Tear down
+$fs = nbFileSystem::getInstance();
+$fs->delete($beeYaml);
+$fs->delete($configYaml);
+$fs->rmdir($configDir);
+
+$t->ok($cmd->run(new nbCommandLineParser(), sprintf('--type=%s %s', $projectType,nbConfig::get('nb_sandbox_dir')), 'Command nbGenerateProjectCommand called successfully'));
+
+$t->ok(file_exists($beeYaml), 'bee.yml added to the destination dir :' . $beeYaml);
+$t->ok(file_exists($configYaml), 'config.yml added to the destination dir :' . $configYaml);
+
+$configParser = sfYaml::load($beeYaml);
+$t->is($configParser['project']['type'], $projectType,'project type '.$projectType.' set successfully');
 
 // Tear down
 $fs = nbFileSystem::getInstance();
