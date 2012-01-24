af135a3c97958dfb60b9afd68c6a9fa37b089b8f
Pietrino Atzeni <pietrino.atzeni@di-side.com>
2012-01-24 09:18:42 +0100

    Changed output when asking confirmation

--- plugins/nbRepositoryPlugin/command/repository/nbRepositoryCommitCommand.php
+++ plugins/nbRepositoryPlugin/command/repository/nbRepositoryCommitCommand.php
@@ -136,7 +136,7 @@ TXT
 
   private function askConfirmation($message)
   {
-    fwrite(STDOUT, $message . "\n");
+    $this->logLine($message, nbLogger::QUESTION);
     $response = fgets(STDIN);
     if(strtolower(trim($response)) != 'y') {
       return false;
