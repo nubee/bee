d2b5ad00a77e1783857df14250d88fe9470bc400
Nicola Piras <nicola.piras.xyz@gmail.com>
2012-04-11 17:32:34 +0200

    New  symfony:init

---
+++ plugins/nbSymfonyPlugin/command/symfony/nbSymfonyInitCommand.php
@@ -0,0 +1,118 @@
+<?php
+
+class nbSymfonyInitCommand extends nbApplicationCommand {
+
+  protected function configure() {
+    $this->setName('symfony:init')
+            ->setBriefDescription('Initliazes a symfony website project (creates and restores database, makes directories for symfony application)')
+            ->setDescription(<<<TXT
+The <info>{$this->getFullName()}</info> command:
+
+  <info>./bee {$this->getFullName()}</info>
+TXT
+    );
+
+    $this->setArguments(new nbArgumentSet(array(
+        new nbArgument('deploy-dir', nbArgument::REQUIRED, 'The production application directory (ie: /var/www/website.com, /var/www/website.com/subdomains/beta)')
+      )));
+
+    $this->setOptions(new nbOptionSet(array(
+        new nbOption('change-web-dir', '', nbOption::PARAMETER_REQUIRED, 'Changes the name of the web directory (if not specified default is "httpdocs")'),
+        new nbOption('change-sf-dir', '', nbOption::PARAMETER_REQUIRED, 'Changes the name of the symfony directory (if not specified default is "symfony")'),
+        new nbOption('db-name', '', nbOption::PARAMETER_REQUIRED, 'If specified creates the database'),
+        new nbOption('db-user', '', nbOption::PARAMETER_REQUIRED, 'The user of the database (requires --db-name)'),
+        new nbOption('db-pass', '', nbOption::PARAMETER_REQUIRED, 'The password for the user of the database (requires --db-name and --db-user)'),
+        new nbOption('db-dump-file', '', nbOption::PARAMETER_REQUIRED, 'Dump file used to populate the database (requires --db-name)'),
+        new nbOption('mysql-user', '', nbOption::PARAMETER_OPTIONAL, 'The mysql root user', 'root'),
+        new nbOption('mysql-pass', '', nbOption::PARAMETER_OPTIONAL, 'The mysql root password', ''),
+      )));
+  }
+
+  protected function execute(array $arguments = array(), array $options = array()) {
+    $this->logLine('Initialising symfony website', nbLogger::COMMENT);
+
+    // bee project must be defined
+    if(!is_dir('./.bee') && !file_exists('./bee.yml')) {
+      $message = 'No bee project defined!';
+      $message .= "\n\n  Run: bee bee:generate-project";
+
+      throw new InvalidArgumentException($message);
+    }
+    
+    // Enable required plugins for website:deploy
+    $cmd = new nbEnablePluginCommand();
+    $cmdLine = 'nbArchivePlugin -f';
+    $this->executeCommand($cmd, $cmdLine, true, false);
+    $cmdLine = 'nbFileSystemPlugin -f';
+    $this->executeCommand($cmd, $cmdLine, true, false);
+    $cmdLine = 'nbMysqlPlugin -f';
+    $this->executeCommand($cmd, $cmdLine, true, false);
+    $cmdLine = 'nbSymfonyPlugin -f';
+    $this->executeCommand($cmd, $cmdLine, true, false);
+    
+    $this->executeShellCommand('rm -f ./.bee/archive-*');
+    $this->executeShellCommand('rm -f ./.bee/filesystem-*');
+    $this->executeShellCommand('rm -f ./.bee/mysql-*');
+    
+    // Makes app directory
+    $deployDir = nbFileSystem::sanitizeDir($arguments['deploy-dir']);
+    $webDir = isset($options['change-web-dir']) ? $options['change-web-dir'] : 'httpdocs';
+    $webDir = sprintf('%s/%s', $deployDir, $webDir);
+    
+    if (!is_dir($webDir)) {
+      $this->getFileSystem()->mkdir($webDir, true);
+    }
+    
+    $symfonyDir = isset($options['change-sf-dir']) ? $options['change-sf-dir'] : 'symfony';
+    $symfonyDir = sprintf('%s/%s', $deployDir, $symfonyDir);
+    
+    if (!is_dir($symfonyDir)) {
+      $this->getFileSystem()->mkdir($symfonyDir, true);
+    }
+
+    // Creates the database
+    $dbName = isset($options['db-name']) ? $options['db-name'] : null;
+    $dbUser = isset($options['db-user']) ? $options['db-user'] : null;
+    $dbPass = isset($options['db-pass']) ? $options['db-pass'] : null;
+    $mysqlUser = isset($options['mysql-user']) ? $options['mysql-user'] : 'root';
+    $mysqlPass = isset($options['mysql-pass']) ? $options['mysql-pass'] : '';
+    
+    if ($dbName && $dbUser && $dbPass) {
+      $cmdLine = sprintf('%s %s %s --username=%s --password=%s', $dbName, $mysqlUser, $mysqlPass, $dbUser, $dbPass);
+      $cmd = new nbMysqlCreateCommand();
+      $this->executeCommand($cmd, $cmdLine, true, false);
+    }
+    
+    // Restores the database
+    $dbDumpFile = isset($options['db-dump-file']) ? $options['db-dump-file'] : null;
+    
+    if (is_file($dbDumpFile)) {
+      if (!$dbName) {
+        $this->logLine('<error>You must specify the database name (use option: --db-name)</error>');
+        return false;
+      }
+      
+      $cmd = new nbMysqlRestoreCommand();
+      $cmdLine = sprintf('%s %s %s %s', $dbName, $dbDumpFile, $mysqlUser, $mysqlPass);
+      $this->executeCommand($cmd, $cmdLine, true, false);
+    }
+    
+    $this->logLine('Symfony website initialized successfully');
+
+    return true;
+  }
+
+  private function executeCommand(nbCommand $command, $commandLine, $doit, $verbose) {
+    if ($doit) {
+      $parser = new nbCommandLineParser();
+      $parser->setDefaultConfigurationDirs($this->getParser()->getDefaultConfigurationDirs());
+
+      if (!$command->run($parser, $commandLine))
+        throw new Exception('Error executing: ' . $cmd);
+    }
+
+    if ($verbose)
+      $this->logLine(sprintf("  <comment>Executing command: %s</comment>\n   %s\n", $command->getFullName(), $commandLine));
+  }
+
+}
