07352731a3c44571cbde5336ffad3b7fd45e6b5a
Nicola Piras <nicola.piras@di-side.com>
2012-11-29 17:00:42 +0100

    Added pdf:text command

---
+++ plugins/nbPdfPlugin/command/pdf/nbPdfToTextCommand.php
@@ -0,0 +1,41 @@
+<?php
+
+class nbPdfToTextCommand extends nbCommand
+{
+
+    protected function configure()
+    {
+        $this->setName('pdf:text')
+            ->setBriefDescription('Extract text from a pdf file')
+            ->setDescription(<<<TXT
+The <info>{$this->getFullName()}</info> command:
+
+  <info>./bee {$this->getFullName()}</info>
+TXT
+        );
+
+        $this->setArguments(new nbArgumentSet(array(
+            new nbArgument('pdf-file', nbArgument::REQUIRED, 'The pdf file'),
+        )));
+
+        $this->setOptions(new nbOptionSet(array(
+        )));
+    }
+
+    protected function execute(array $arguments = array(), array $options = array())
+    {
+        $file = $arguments['pdf-file'];
+
+        $this->logLine(sprintf('Extracting text from pdf file: %s', $file), nbLogger::COMMENT);
+
+        $pdf2text = new PDF2Text();
+        $pdf2text->setFilename($file);
+        $pdf2text->decodePDF();
+        $output = $pdf2text->output();
+
+        file_put_contents(sprintf('%s.txt', $file), $output);
+
+        $this->logLine('Text extracted!', nbLogger::COMMENT);
+    }
+
+}
